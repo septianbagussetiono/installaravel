fc0564b58476a6928f245210f16fcf0a82119f3b
septianbagussetiono <septianbagus275@gmail.com>
2022-01-05 13:12:08 +0700

    tugas 2 blade

--- installaravel/routes/web.php
+++ installaravel/routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +12,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.dashboard');
 });
