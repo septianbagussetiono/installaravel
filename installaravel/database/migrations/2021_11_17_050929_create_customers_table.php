39fcb0df21952ef4e39240200a8b4dda60be2854
septianbagussetiono <septianbagus275@gmail.com>
2022-01-05 13:25:39 +0700

    tugas 3 migrasi

---
+++ installaravel/database/migrations/2021_11_17_050929_create_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id()->unsigned();
+            $table->char('email', 191);
+            $table->char('name', 191);
+            $table->text('addres');
+            $table->char('phone', 191);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
